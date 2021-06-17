381735bd6f8b30a60b29368c9353421665a7f01f
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-17 20:01:26 -0300

    adiciona mais exercicios a parte 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios/Dia_1/exercice2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios/Dia_1/exercice2.js
@@ -13,6 +13,7 @@ const fatorial = numero => {
 
 console.log(fatorial(7));
 
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
 const longestWord = string => {
     let palavras = string.split(' ');
     let result = '';
@@ -24,3 +25,32 @@ const longestWord = string => {
     return result;
 }
 console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")) // retorna 'aconteceu';
+
+//Crie uma página que contenha:
+//Um botão ao qual será associado um event listener ;
+//Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+//Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+
+let clickCount = 0;
+const contador = document.querySelector('#contador');
+const button = document.querySelector('#buttonContator')
+
+button.addEventListener('click', () => {
+    clickCount += 1;
+    contador.innerText = `Numeros de cliques: ${clickCount}`;
+})
+
+//Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar 
+// e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+//Exemplo:
+//String determinada: "Tryber x aqui!"
+//Parâmetro: "Bebeto"
+//Retorno: "Tryber Bebeto aqui!"
+//Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+//Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que 
+// foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+//Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+//JavaScript;
+//HTML; ... #goTrybe".
+
+
