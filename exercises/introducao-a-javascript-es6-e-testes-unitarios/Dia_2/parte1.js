37d1ce7ddc094ee44b603bb90fb286d84434d476
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-18 20:04:28 -0300

    Adiciona primeira parte

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios/Dia_2/parte1.js
@@ -0,0 +1,54 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        margherita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+
+//Agora você vai fazer alguns exercícios de fixação.
+//Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+//Note que o parâmetro da função já está sendo passado na chamada da função.
+  
+
+  const customerInfo = order => { 
+      console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, N° ${order.address.number} ,AP:${order.address.apartment}`);
+    // Adicione abaixo as informações necessárias.
+  }
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  }
+  
+  orderModifier(order);
